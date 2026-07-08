4e9032c6d2add548da60d85b1a436d47471aee09
amir_shk <shaikhamirarif1998@gmail.com>
2026-07-08 15:39:46 +0530

    Time: 8 ms (9.86%), Space: 47.7 MB (5.44%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
@@ -3,41 +3,20 @@ class Solution {
     int []arr=new int[2];;
     Arrays.sort(nums1);
     Arrays.sort(nums2);
-    int i=0;
-    int j=0;
-    while(i<nums1.length && j<nums2.length){
-        if(nums1[i]==nums2[j]){
-            arr[0]++;
-            arr[1]++;
-            i++;
-            j++;
-        }
-        else if(nums1[i]<nums2[j]){
-            if( j>0 && nums1[i]==nums2[j-1]){
-                arr[0]++;
-            }
-            i++;
-        }
-        else {
-            if(i>0 && nums1[i-1]==nums2[j] ){
-                arr[1]++;
-            }
-            j++;
-        }    
-        }
-        while(i<nums1.length){
-            if(nums1[i]==nums2[j-1]){
-                arr[0]++;
-                }
-            i++;    
-        }
-         while(j<nums2.length){
-            if(nums2[j]==nums1[i-1]){
-                arr[1]++;
-            }
-            j++;
-        }
+    boolean []arr1=new boolean[101];
+    boolean []arr2=new boolean[101];
+    for(int el:nums1){
+        arr1[el]=true;
+    }
+    for(int el:nums2){
+        arr2[el]=true;
+    }
+    for(int el:nums1){
+        if(arr2[el]) arr[0]++;
+    }
+    for(int el:nums2){
+        if(arr1[el]) arr[1]++;
+    }
     return arr;
     }
 }
-
