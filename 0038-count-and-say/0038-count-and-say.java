f26406d1921181b6c389bed01079d956bc9d38a2
amir_shk <shaikhamirarif1998@gmail.com>
2026-07-09 16:12:04 +0530

    Time: 2 ms (99.63%), Space: 42.8 MB (92.3%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -4,20 +4,19 @@ class Solution {
         return "1";
        } 
        String s1=countAndSay(n-1);
-       String result="";
+       StringBuilder result=new StringBuilder();
        int i=0;
-       int count=1;
-       while(i<s1.length()-1){
-        if(s1.charAt(i)!=s1.charAt(i+1)){
-            result+=Integer.toString(count)+Character.toString(s1.charAt(i));
-            count=1;}
-        else{
-            count++;
-        }
-        i++;
+       while(i<s1.length()){
+          char ch=s1.charAt(i);
+          int freq=0;
+          while(i<s1.length() && s1.charAt(i)==ch ){
+            i++;
+            freq++;
+          }
+         result.append(freq);
+          result.append(ch);
        }
-       result+=Integer.toString(count)+Character.toString(s1.charAt(i));
-       return result;
+       return result.toString();
 
     }
 }
