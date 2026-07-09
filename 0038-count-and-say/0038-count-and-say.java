9508123db17730061f4aeb56b2ad9789c82f8a43
amir_shk <shaikhamirarif1998@gmail.com>
2026-07-09 16:03:23 +0530

    Time: 12 ms (13.78%), Space: 47.1 MB (9.18%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,28 +1,23 @@
 class Solution {
     public String countAndSay(int n) {
-        if (n == 1)
-            return "1";
-
-        String str = countAndSay(n - 1);
-
-        //now do the actual RLE on str
-        int index = 0;
-        StringBuilder sb = new StringBuilder();
-
-        while (index < str.length()) {
-            char ch = str.charAt(index);
-            int freq = 0;
-
-            while(index < str.length() && str.charAt(index) == ch) {
-                index++;
-                freq++;
-            }
-
-            //now form the sb
-            sb.append((char) ('0' + freq));
-            sb.append(ch);
+       if(n==1){
+        return "1";
+       } 
+       String s1=countAndSay(n-1);
+       String result="";
+       int i=0;
+       int count=1;
+       while(i<s1.length()-1){
+        if(s1.charAt(i)!=s1.charAt(i+1)){
+            result+=Integer.toString(count)+Character.toString(s1.charAt(i));
+            count=1;}
+        else{
+            count++;
         }
+        i++;
+       }
+       result+=Integer.toString(count)+Character.toString(s1.charAt(i));
+       return result;
 
-        return sb.toString();
     }
 }
