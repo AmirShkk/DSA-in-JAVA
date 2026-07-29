5024e31f23147533e090a21593b4a5d1cb93141f
amir_shk <shaikhamirarif1998@gmail.com>
2026-07-29 20:45:42 +0530

    Time: 0 ms (100%), Space: 44.4 MB (31.09%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -9,18 +9,28 @@
  * }
  */
 class Solution {
+    public ListNode recursion(ListNode head){
+        if(head.next==null) return head;
+        ListNode a=head.next;
+        ListNode b=recursion(a);
+        a.next=head;
+        head.next=null;
+        return b;
+    }
     public ListNode reverseList(ListNode head) {
-     if(head==null || head.next==null) return head;
-     ListNode curr=head;
-     ListNode prev=null;
-     ListNode forward=null;
-     while(curr.next!=null){
-        forward=curr.next;
-        curr.next=prev;
-        prev=curr;
-        curr=forward;
-        }
-    curr.next=prev;
-    return curr;
+        if(head==null || head.next==null) return head;
+    //  if(head==null || head.next==null) return head;
+    //  ListNode curr=head;
+    //  ListNode prev=null;
+    //  ListNode forward=null;
+    //  while(curr.next!=null){
+    //     forward=curr.next;
+    //     curr.next=prev;
+    //     prev=curr;
+    //     curr=forward;
+    //     }
+    // curr.next=prev;
+    // return curr;
+    return recursion(head);
     }
 }
