7afaf167d849fe83ebe8ad972c1464dc9c386293
amir_shk <shaikhamirarif1998@gmail.com>
2026-08-09 01:09:45 +0530

    Time: 1 ms (42.52%), Space: 43.3 MB (75.94%) - LeetHub

---
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
@@ -0,0 +1,26 @@
+class Solution {
+    public Node recursion(Node head) {
+        if (head == null) return head;
+        Node curr = head;
+        while (curr != null) {
+            if (curr.child == null) {
+                curr = curr.next;
+            } else {
+                Node forward = curr.next;
+                Node pointer = recursion(curr.child);
+                curr.child = null;
+                curr.next = pointer;
+                pointer.prev = curr;
+                while (curr.next != null) curr = curr.next;
+                curr.next = forward;
+                if (forward != null) forward.prev = curr;
+                curr = curr.next;
+            }
+        }
+        return head;
+    }
+
+    public Node flatten(Node head) {
+        return recursion(head);
+    }
+}
