a3bdc6cf4462ba29e35b7e441d206e816688ea18
amir_shk <shaikhamirarif1998@gmail.com>
2026-08-09 13:25:12 +0530

    Time: 4 ms (100%), Space: 106 MB (72.31%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -1,37 +1,38 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-    int [] arr=new int[2];    
-    if(head==null && head.next==null && head.next.next==null) return arr;
-    ListNode temp=head;
-    int prevval=head.val;
-    int min=Integer.MAX_VALUE;
-    int index=-1;
-    int firstpoint=0;
-    int count=0;
-    while(temp.next!=null){
-     if((temp.val>prevval && temp.val>temp.next.val) || (temp.val<prevval && temp.val<temp.next.val)){
-       if(index==-1) firstpoint=count;
-       else{
-        if(count-index<min) min=count-index;
+        
+        int[] ans = {-1,-1};
+        if(head == null || head.next == null || head.next.next == null) {
+            return ans;
         }
-        index=count;
-    }
-    prevval=temp.val;
-    count++;
-    temp=temp.next;
-    }
-    arr[0]=(min!=Integer.MAX_VALUE)?min:-1;
-    arr[1]=(index-firstpoint>0)?(index-firstpoint):-1;
-    return arr;    
+        ListNode a = head;
+        ListNode b = a.next;
+        ListNode c = b.next;
+        int minDist = Integer.MAX_VALUE;
+        int firstIdx = -1;
+        int lastIdx = -1;
+        int idx =1;
+        while(c != null){
+            if(b.val > c.val && b.val > a.val ||b.val < c.val && b.val < a.val ){
+                if(firstIdx == -1) firstIdx = idx;
+                if(lastIdx != -1){
+                    int dist = idx - lastIdx;
+                    minDist = Math.min(minDist,dist);
+                }
+                lastIdx = idx;
+                
+            }
+           idx++;
+           a = a.next;
+           b =  b.next;
+           c = c.next; 
+
         }
+        int maxDist = lastIdx - firstIdx;
+        if(maxDist == 0) maxDist = -1;
+        if(minDist == Integer.MAX_VALUE) minDist = -1;
+        ans[0] = minDist;
+        ans[1] = maxDist;
+        return ans;
+    }
 }
