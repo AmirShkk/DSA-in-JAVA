6ab52c56bd7acd1214762daa3200c08856183341
amir_shk <shaikhamirarif1998@gmail.com>
2026-08-14 11:09:12 +0530

    Time: 71 ms (32.74%), Space: 134.2 MB (51.86%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -8,34 +8,54 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public ListNode reverse(ListNode head){
-       ListNode curr=head;
-       ListNode prev=null;
-       ListNode forward=null;
-    while(curr!=null){
-        forward=curr.next;
-        curr.next=prev;
-        prev=curr;
-        curr=forward;
-       }
-       return prev;
-    }  
+// class Solution {
+//     public ListNode reverse(ListNode head){
+//        ListNode curr=head;
+//        ListNode prev=null;
+//        ListNode forward=null;
+//     while(curr!=null){
+//         forward=curr.next;
+//         curr.next=prev;
+//         prev=curr;
+//         curr=forward;
+//        }
+//        return prev;
+//     }  
+//     public ListNode removeNodes(ListNode head) {
+//     ListNode reversehead=reverse(head);    
+//     ListNode i=new ListNode(0);
+//     ListNode temp=i;
+//     ListNode j=reversehead;
+//     while(j!=null){
+//         if(j.val<temp.val){} 
+//         else {
+//             System.out.println(j.val);
+//             temp.next=j;
+//             temp=temp.next;
+//           }
+//         j=j.next;
+//     }
+//     temp.next=null;
+//     return reverse(i.next);
+//   }
+// }
+// using stack
+class Solution{
     public ListNode removeNodes(ListNode head) {
-    ListNode reversehead=reverse(head);    
-    ListNode i=new ListNode(0);
-    ListNode temp=i;
-    ListNode j=reversehead;
-    while(j!=null){
-        if(j.val<temp.val){} 
-        else {
-            System.out.println(j.val);
-            temp.next=j;
+        Stack<ListNode> st=new Stack<>();
+        ListNode temp=head;
+        while(temp!=null){
+            while(st.size()!=0 && temp.val>st.peek().val){
+                st.pop();
+            }
+            st.push(temp);
             temp=temp.next;
-          }
-        j=j.next;
+        }
+        while(st.size()>0){
+          ListNode t=st.pop();
+          t.next=temp;
+          temp=t;
+        }
+      return temp;
     }
-    temp.next=null;
-    return reverse(i.next);
-  }
 }
